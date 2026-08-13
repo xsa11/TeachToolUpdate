c18eafbe467aff5cf367a409a5aa84ebd14f61ee
xsa <1245351933@qq.com>
2026-08-13 15:46:42 +0800

    添加提示

--- UpdateTeach/obj/Debug/net8.0-windows/UpdateTeach.AssemblyInfo.cs
+++ UpdateTeach/obj/Debug/net8.0-windows/UpdateTeach.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UpdateTeach")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a7b18f86f2a99d7e44a9f254e6540aa36e1f88")]
 [assembly: System.Reflection.AssemblyProductAttribute("UpdateTeach")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UpdateTeach")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
