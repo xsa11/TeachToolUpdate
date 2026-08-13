863473ab6a712e3eabf0e9d54f433c9fde98be67
xsa <1245351933@qq.com>
2026-08-13 16:46:31 +0800

    test

--- UpdateTeach/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ UpdateTeach/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -269,7 +269,7 @@ namespace UpdateTeach {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/UpdateTeach;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/UpdateTeach;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
